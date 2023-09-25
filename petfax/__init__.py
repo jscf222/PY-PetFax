124a005d090b7c59fa82a100647ab823adf82185
jscf222 <jscf222@gmail.com>
2023-09-25 15:06:16 -0700

    working

---
+++ petfax/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask 
+
+def create_app(): 
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+    
+
+    # register pet blueprint
+    from . import pet
+    app.register_blueprint(pet.bp)
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+   
+    
+
+    return app
